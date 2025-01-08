238157ba5f9b25415ab86338ecacdc34b55c78a0
Svatoslav Kyshiei <_@example.com>
2025-01-08 21:43:06 +0200

    останній коміт

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-import telebot
 import requests
 import json
+import telebot
 
-bot = telebot.TeleBot("7985806779:AAECH7S44hWattalE25ypuSx3Mmbapkn5sQ")
-api = "43723760989e623ad27b0bb45a714ff9"
+bot = telebot.TeleBot('7985806779:AAECH7S44hWattalE25ypuSx3Mmbapkn5sQ')
+api = '43723760989e623ad27b0bb45a714ff9'
 
 @bot.message_handler(commands=['start'])
-def start_bot(message):
-    bot.send_message(message.chat.id,"Привіт,напиши назву свого міста!")
-
+def start_message(message):
+    bot.send_message(message.chat.id,"Привіт напиши назву свого міста 😁")
 
 @bot.message_handler(content_types=['text'])
-def get_weathher(message):
+def get_weather(message):
     city = message.text.strip().lower()
     res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=metric')
     data = json.loads(res.text)
-    bot.reply_to(message,f'Зараз погода: {data['main']['temp']} градусів')
 
-bot.polling(none_stop=True)
+    if res.status_code == 200:
+        # Якщо місто знайдено, виводимо температуру
+        bot.reply_to(message, f'Зараз погода в місті {city.capitalize()}: {data["main"]["temp"]} градусів')
+    else:
+        # Якщо місто не знайдено або інша помилка
+        bot.reply_to(message, f'Не вдалося знайти місто "{city.capitalize()}". Будь ласка, перевірте правильність введеного назви 😒w')
+
+
+bot.polling(non_stop=True)
